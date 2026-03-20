7b6ace551fe6225f418c7ae2a7f0af87a7e92cd2
Dan <m41n4dunc4n@gmail.com>
2026-03-20 23:19:00 +0300

    removed tensorflow

--- main.py
+++ main.py
@@ -392,5 +392,7 @@ def logout():
 
 # ---------------- RUN ----------------
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
